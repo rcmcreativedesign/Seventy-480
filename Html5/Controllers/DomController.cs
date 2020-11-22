2468c93951a9e3f1035e97a14e042002df17dbd3
Robert McDonnell <rcmcd@hotmail.com>
2020-11-21 23:16:45 -0700

    Added examples in html format.

---
+++ Html5/Controllers/DomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Html5.Controllers
+{
+    public class DomController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
